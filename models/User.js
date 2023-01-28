401817a4b33b60289657a29b71c79fdf0360f884
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2023-01-28 16:52:04 +0100

    updated user model

--- models/User.js
+++ models/User.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    role: { type: String, required: true },
+    created: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
