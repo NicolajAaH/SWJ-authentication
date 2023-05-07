d59e2dea54d09193a3a049afab34fd9e10d388eb
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2023-05-07 21:55:13 +0200

    added test coverage to testing

--- test/server.test.js
+++ test/server.test.js
@@ -38,6 +38,24 @@ describe('User authentication', () => {
     });
   });
 
+  describe('/register', () => {
+    it('Should not register a new user with no email', done => {
+      chai
+        .request(app)
+        .post('/register')
+        .send({
+          email: 'test@example.com',
+          name: 'User',
+          role: 'FRONTEND'
+        })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(500);
+          done();
+        });
+    });
+  });
+
   describe('POST /login', () => {
     it('should return 200 and a JSON Web Token on successful login', async () => {
       chai.request(app)
@@ -81,17 +99,91 @@ describe('User authentication', () => {
   }
   );
 
+  describe('Get user endpoint', () => {
+    it('Should return a user', (done) => {
+      chai.request(app)
+        .get('/user/5d9f1140f10a81216cfd4408')
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.body[0].email).to.equal('test@example.com');
+          expect(res.body[0]._id).to.equal('5d9f1140f10a81216cfd4408');
+          expect(res.body[0].role).to.equal('COMPANY');
+          expect(res.body[0].name).to.equal('User');
+          done();
+        });
+    });
+  }
+  );
+
+  describe('Put user endpoint', () => {
+    it('Should put a user', (done) => {
+      chai.request(app)
+        .put('/user/5d9f1140f10a81216cfd4408')
+        .send({
+          email: 'test@example.com',
+          password: '123456789',
+          name: 'User',
+          role: 'FRONTEND'
+        })
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.body).to.be.an('object');
+          done();
+        });
+    });
+  }
+  );
+
+  describe('Put user endpoint', () => {
+    it('Should not put an empty user', (done) => {
+      chai.request(app)
+        .put('/user/')
+        .send({
+        })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(404);
+          done();
+        });
+    });
+  }
+  );
+
   describe('POST /logout', () => {
     it('should return okay when calling logout', async () => {
       chai.request(app)
-      .post('/logout')
-      .send({
-        email: 'test@example.com'
-      }).end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-      });
+        .post('/logout')
+        .send({
+          email: 'test@example.com'
+        }).end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+        });
     });
   });
 
+  describe('DELETE /user', () => {
+    it('should return okay when calling delete', async () => {
+      chai.request(app)
+        .delete('/user/5d9f1140f10a81216cfd4408')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+        }
+        );
+    });
+
+    describe('DELETE /user', () => {
+      it('should return error when not calling with ID', async () => {
+        chai.request(app)
+          .delete('/user/')
+          .end((err, res) => {
+            expect(err).to.be.null;
+            expect(res).to.have.status(404);
+          }
+          );
+      });
+
+  });
+});
 });
